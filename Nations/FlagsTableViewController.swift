75ccc836448ff02baaac42bdd12bfbc55838022b
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-08-02 23:12:41 +0300

    prepare template

---
+++ Nations/FlagsTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FlagsTableViewController.swift
+//  Nations
+//
+//  Created by newbie on 02.08.2022.
+//
+
+import UIKit
+
+class FlagsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
